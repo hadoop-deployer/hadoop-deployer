fb26638f5ab9e032704ccf4c0cbfadefb8499319
zhaigy <zhaigy@ucweb.com>
2012-09-27 15:16:49 +0800

    fix

--- cdh3/uninstall_all.sh
+++ cdh3/uninstall_all.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
-main()
-{
-  . PUB.sh
-  [ -f ./install_env.sh ] && : || die "install_env.sh file not exits";
-  echo ">> uninstall hue"
-  sh uninstall_hue.sh
-  echo ">> uninstall hbase"
-  sh uninstall_hbase.sh
-  echo ">> uninstall hive"
-  sh uninstall_hive.sh
-  echo ">> uninstall hadoop"
-  sh uninstall_hadoop.sh
-}
+. PUB.sh
+[ -f ./install_env.sh ] && : || die "install_env.sh file not exits";
+echo ">> uninstall zookeeper"
+sh uninstall_zookeeper.sh
+echo ">> uninstall hue"
+sh uninstall_hue.sh
+echo ">> uninstall hbase"
+sh uninstall_hbase.sh
+echo ">> uninstall hive"
+sh uninstall_hive.sh
+echo ">> uninstall hadoop"
+sh uninstall_hadoop.sh
 
-#====
-main
