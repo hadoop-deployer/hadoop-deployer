ec586f3e90e9041750bfb461f1e63f37b2d24f78
zhaigy <zhaigy@ucweb.com>
2012-11-27 18:07:45 +0800

    fix

---
+++ cdh4/config_hive.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# coding=utf-8
+# Author: zhaigy@ucweb.com
+# Data:   2012-11
+
+# HBase的众多服务端口的前缀，最多2位数，建议2位数
+HIVE_PORT_PREFIX=$PORT_PREFIX
+
+# 把Hive安装到一台机器上
+HIVE_NODE="host1"
+
