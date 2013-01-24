692c47371e9796630b43f8ed94ee987cdd246f07
zhaigy <zhaigy@ucweb.com>
2013-01-24 11:17:27 +0800

    fix

--- cdh4.1/support/hadoop_conf/sbin/start-journalnode.sh
+++ cdh4.1/support/hadoop_conf/sbin/start-journalnode.sh
@@ -12,9 +12,10 @@ DEFAULT_LIBEXEC_DIR="$bin"/../libexec
 HADOOP_LIBEXEC_DIR=${HADOOP_LIBEXEC_DIR:-$DEFAULT_LIBEXEC_DIR}
 . $HADOOP_LIBEXEC_DIR/hdfs-config.sh
 
-# namenodes
-
+edits_dir=$(hdfs getconf -confKey dfs.namenode.shared.edits.dir)
+#qjournal://platform30:44485;platform31:44485;platform32:44485;platform33:44485;platform34:44485/mycluster
+journal_nodes=$(echo ${edits_dir##*://} | sed -e "s:/.*$::" -e "s/:[0-9]\+;\?/ /g")
 "$HADOOP_PREFIX/sbin/hadoop-daemons.sh" \
   --config "$HADOOP_CONF_DIR" \
+  --hostnames "$journal_nodes" \
   --script "$bin/hdfs" start journalnode
-  #--hostnames "$NAMENODES" \
--- cdh4.1/support/hadoop_conf/sbin/stop-zkfc.sh
+++ cdh4.1/support/hadoop_conf/sbin/stop-zkfc.sh
@@ -9,6 +9,8 @@ DEFAULT_LIBEXEC_DIR="$bin"/../libexec
 HADOOP_LIBEXEC_DIR=${HADOOP_LIBEXEC_DIR:-$DEFAULT_LIBEXEC_DIR}
 . $HADOOP_LIBEXEC_DIR/hdfs-config.sh
 
+NAMENODES=$($HADOOP_PREFIX/bin/hdfs getconf -namenodes)
+
 # ZK Failover controllers, if auto-HA is enabled
 AUTOHA_ENABLED=$($HADOOP_PREFIX/bin/hdfs getconf -confKey dfs.ha.automatic-failover.enabled)
 if [ "$(echo "$AUTOHA_ENABLED" | tr A-Z a-z)" = "true" ]; then
